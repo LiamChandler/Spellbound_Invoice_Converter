d445b110cb42aa25dc18e777a21be828fbfb8516
Liam Chandler <LiamDChandler@gmail.com>
2020-03-13 15:37:04 +1300

    Changed CSV parsing to dataTable format to avoid errors with different sized CSV files

--- Spellbound_Invoice_Converter/csvConvert.cs
+++ Spellbound_Invoice_Converter/csvConvert.cs
@@ -5,97 +5,85 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
+using System.Data;
+using System.Text.RegularExpressions;
+
 
 namespace Spellbound_Invoice_Converter
 {
     class csvConvert
     {
-        csvConvert()
-        {
-
-        }
-
         public static void ConvertCSV(String file)
         {
-            // Make variables
-            List<List<String>> lines = new List<List<string>>();
-            String line;
-
-            // Read file contents into 2D list
-            try
-            {
-                StreamReader sr = new StreamReader(file);
-                line = sr.ReadLine();
-
-                while(line != null)
-                {
-                    lines.Add(new List<string>(line.Split(',')));
-                    line = sr.ReadLine();
-                }
-                sr.Close();
-            }
-            catch(Exception e)
-            {
-                Debug.WriteLine(e.StackTrace);
-            }
-
-            //  Sort data into groups by agent
-
+            DataTable t =  ConvertCSVtoDataTable(file);
+            bool added;
+            Client currentClient;
             List<Agent> agents = new List<Agent>();
-            List<List<String>> erroredLines = new List<List<string>>();
 
-            bool firstEntry = true;
-            foreach(List<string> currentLine in lines)
+            foreach(DataRow row in t.Rows)
             {
-                // Check if the first line
-                if (!firstEntry)
+                added = false;
+                currentClient = new Client();
+
+                // Parse Data
+                String[] tmp = ((string)row[t.Columns.IndexOf("Date - ISO")]).Substring(0, 10).Split('-');
+                currentClient.date = new DateTime((int)Int32.Parse(tmp[0]), (int)Int32.Parse(tmp[1]), (int)Int32.Parse(tmp[2]));
+
+                currentClient.orderNumber       = (string)row[t.Columns.IndexOf("Order number")];
+                currentClient.agent             = (string)row[t.Columns.IndexOf("Agent")];
+                currentClient.name              = (string)row[t.Columns.IndexOf("Customer name")];
+                currentClient.paidToAgent       = float.Parse((string)row[t.Columns.IndexOf("Paid to agent")]);
+                currentClient.commission        = float.Parse((string)row[t.Columns.IndexOf("Commission")]);
+                currentClient.amountOwned       = currentClient.paidToAgent - currentClient.commission;
+                currentClient.agentRefernce     = (string)row[t.Columns.IndexOf("Agent reference")];
+
+                if (agents.Count != 0)
                 {
-                    try
+                    foreach(Agent a in agents)
                     {
-                        Client currentClient = new Client();
-
-                        // Parse Data
-                        String[] tmp = currentLine[0].Substring(0,10).Split('-');
-                        currentClient.date = new DateTime((int)Int32.Parse(tmp[0]), (int)Int32.Parse(tmp[1]), (int)Int32.Parse(tmp[2]));
-                        currentClient.orderNumber = currentLine[45];
-                        currentClient.agent = currentLine[6];
-                        currentClient.name = currentLine[16];
-                        currentClient.paidToAgent = float.Parse(currentLine[39]);
-                        currentClient.commission = float.Parse(currentLine[42]);
-                        currentClient.amountOwned = currentClient.paidToAgent - currentClient.commission;
-                        currentClient.agentRefernce = currentLine[45];
-
-                        // Add Client to Agent, if no matching agent exists create one
-                        bool added = false;
-                        if(agents.Count != 0)
+                        if(currentClient.agent.CompareTo(a.Name) == 0)
                         {
-                            foreach(Agent a in agents)
-                            {
-                                if(a.Name.CompareTo(currentClient.agent) == 0)
-                                {
-                                    a.clients.Add(currentClient);
-                                    added = true;
-                                    break;
-                                }
-                            }
+                            a.clients.Add(currentClient);
+                            added = true;
+                            break;
                         }
-                        if (!added)
-                        {
-                            Agent newAgent = new Agent(currentClient.agent);
-                            newAgent.clients.Add(currentClient);
-                            agents.Add(newAgent);
-                        }
-                    }
-                    //  Catch errored lines and add them to the errored list
-                    catch
-                    {
-                        Debug.WriteLine("Could not parse line: " + currentLine);
-                        erroredLines.Add(currentLine);
                     }
                 }
-                else
-                    firstEntry = false;
+                if(!added)
+                {
+                    Agent newAgent = new Agent(currentClient.agent);
+                    newAgent.clients.Add(currentClient);
+                    agents.Add(newAgent);
+                }
             }
+        }
+
+        public static DataTable ConvertCSVtoDataTable(string strFilePath)
+        {
+            DataTable dt = new DataTable();
+            using (StreamReader sr = new StreamReader(strFilePath))
+            {
+                string[] headers = Regex.Split(sr.ReadLine(), @"\s*(?:""(?<val>[^""]*(""""[^""]*)*)""\s*|(?<val>[^,]*))(?:,|$)");
+                foreach (string header in headers)
+                {
+                    dt.Columns.Add(header);
+                }
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = Regex.Split(sr.ReadLine(), @"\s*(?:""(?<val>[^""]*(""""[^""]*)*)""\s*|(?<val>[^,]*))(?:,|$)");
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dr[i] = rows[i];
+                    }
+                    // Add row if agent exists
+                    if (((string)dr[dt.Columns.IndexOf("Agent")]).CompareTo("") != 0)
+                        dt.Rows.Add(dr);
+                }
+
+            }
+            Debug.WriteLine(dt.Rows.Count + " lines found");
+            return dt;
         }
     }
 
